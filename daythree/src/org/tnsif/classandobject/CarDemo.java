e25739ecf8523fd216f488c41e45b8dea62d33f5
MRUNALI PATIL <MRUNALI PATIL@DESKTOP-M2BC7S8>
2023-02-08 13:46:56 +0530

    first commit

---
+++ daythree/src/org/tnsif/classandobject/CarDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.classandobject;
+
+public class CarDemo {
+
+	public static void main(String[] args) {
+		
+		//object creation
+		Car c=new Car();
+		//calling the method
+		c.display(45);
+
+	}
+
+}
